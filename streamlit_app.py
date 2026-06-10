8e94c76d838ea9108f2e1b1f09aba173dddb29df
Harshini Nitturi <nitturiharshini@gmail.com>
2026-06-10 17:15:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,16 @@ st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 name_on_order = st.text_input("Name on Smoothie:")
 
 st.write("The name on your Smoothie will be:", name_on_order)
+if ingredients_list:
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        
+        search_on = fruit_options.loc[fruit_options['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Snowflake Session
 session = get_active_session()
